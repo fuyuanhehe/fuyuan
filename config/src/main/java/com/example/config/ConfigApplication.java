d16607e0f68590069df21a7e1e82a1a87763662c
fuyuan <1024767308@qq.com>
2018-08-14 09:30:50 +0800

    11

--- config/src/main/java/com/example/config/ConfigApplication.java
+++ config/src/main/java/com/example/config/ConfigApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer   // 开启配置服务
 @EnableDiscoveryClient  //注册config服务到eureka
 public class ConfigApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
     }
