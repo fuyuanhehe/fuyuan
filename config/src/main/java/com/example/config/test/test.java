585989ebc8fa3554d399d815c9fdbf7c6508bdef
fuyuan <1024767308@qq.com>
2018-08-14 11:31:01 +0800

    测试4

--- config/src/main/java/com/example/config/test/test.java
+++ config/src/main/java/com/example/config/test/test.java
@@ -6,4 +6,7 @@ public class test {
 	// http://localhost:8001/neo-config/dev
 	
 	// http://localhost:8001/neo-config-dev.properties   访问具体文件
+	
+	
+	// curl -X POST http://localhost:8001/bus/refresh  cmd 刷新后，所有客户端更新配置
 }
