ba040aed6a80ba6d21c59d9802518499a14727c0
fuyuan <1024767308@qq.com>
2018-08-14 09:42:02 +0800

    测试

---
+++ config/src/main/java/com/example/config/test/test.java
@@ -0,0 +1,9 @@
+package com.example.config.test;
+
+public class test {
+
+	
+	// http://localhost:8001/neo-config/dev
+	
+	// http://localhost:8001/neo-config-dev.properties   访问具体文件
+}
---
+++ mo/src/main/java/com/example/mo/test/test.java
@@ -0,0 +1,8 @@
+package com.example.mo.test;
+
+public class test {
+
+	// http://localhost:8002/hello 客户端访问路径
+	
+	
+}
